161d7419a119aeb264873fdd172333764424c950
mtgrinstead <michael@mgrinstead.com>
2024-07-06 21:51:20 -0400

    removing pokedexcli binary

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
     "strings"
 )
 
+// Names CliName and calls function to print it
+var cliName = "Pokedex" 
+func printPrompt() {
+    fmt.Print(cliName, ">")
+}
+
+//prints if an unknown function is typed
+func printUnknown() {
+    fmt.Println(text, ": command not found")
+}
+
+type cliCommand struct {
+    name        string
+    description string
+    callback    func() error
+}
+
+
+
 func main() {
 	fmt.Println("Pokedex >")
 }
