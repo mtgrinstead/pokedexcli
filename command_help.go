fcc005e841e0c33f26e4a1c6e0c5f100bb5bf56f
mtgrinstead <michael@mgrinstead.com>
2024-07-12 01:03:26 -0400

    finished explore func

--- command_help.go
+++ command_help.go
@@ -4,7 +4,7 @@ import "fmt"
  
 
 
-func callbackHelp(cfg *config) error {
+func callbackHelp(cfg *config, args ...string) error {
     fmt.Println("Welcome to the Pokedex help menu.")
     fmt.Println("Here are your available commands:")
 
--- command_map.go
+++ command_map.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "errors"
+	"errors"
+	"fmt"
 )
 
-func callbackMap(cfg *config) error {
-    resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
-    if err != nil {
-        return err
-    }
-    fmt.Println("Location areas:")
-    for _, area := range resp.Results {
-        fmt.Printf(" - %s\n", area.Name)
-    }
-    cfg.nextLocationAreaURL = resp.Next
-    cfg.prevLocationAreaURL = resp.Previous
-    return nil
+func callbackMap(cfg *config, args ...string) error {
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Location areas:")
+	for _, area := range resp.Results {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+	cfg.nextLocationAreaURL = resp.Next
+	cfg.prevLocationAreaURL = resp.Previous
+	return nil
 }
 
-func callbackMapb(cfg *config) error {
-    if cfg.prevLocationAreaURL == nil {
-        return errors.New("you're on the first page")
-    }
-    resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.prevLocationAreaURL)
-    if err != nil {
-        return err
-    }
-    fmt.Println("Location areas:")
-    for _, area := range resp.Results {
-        fmt.Printf(" - %s\n", area.Name)
-    }
-    cfg.nextLocationAreaURL = resp.Next
-    cfg.prevLocationAreaURL = resp.Previous
-    return nil
+func callbackMapb(cfg *config, args ...string) error {
+	if cfg.prevLocationAreaURL == nil {
+		return errors.New("you're on the first page")
+	}
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.prevLocationAreaURL)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Location areas:")
+	for _, area := range resp.Results {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+	cfg.nextLocationAreaURL = resp.Next
+	cfg.prevLocationAreaURL = resp.Previous
+	return nil
 }
