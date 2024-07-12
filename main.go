7382845f3abf7dafdcb93a6d4af78cdc2cb1e3ea
mtgrinstead <michael@mgrinstead.com>
2024-07-12 19:04:07 -0400

    added catch functions

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 )
 
 type config struct {
-    pokeapiClient           pokeapi.Client
-    nextLocationAreaURL     *string
+    pokeapiClient       pokeapi.Client
+    nextLocationAreaURL *string
     prevLocationAreaURL *string
+    caughtPokemon       map[string]pokeapi.Pokemon
 }
 
 func main() {
     cfg := config{
-      pokeapiClient: pokeapi.NewClient(time.Hour),  
+      pokeapiClient: pokeapi.NewClient(time.Hour),
+      caughtPokemon: make(map[string]pokeapi.Pokemon), 
     }
     startRepl(&cfg)
 }
--- repl.go
+++ repl.go
@@ -1,85 +1,89 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func startRepl(cfg *config) {
 
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    for {
-        fmt.Print("Pokedex >")
+	for {
+		fmt.Print("Pokedex >")
 
-        scanner.Scan()
-        text := scanner.Text()
+		scanner.Scan()
+		text := scanner.Text()
 
-        cleaned := cleanInput(text)
-        if len(cleaned) == 0 {
-            continue
-        }
-        commandName := cleaned[0]
-        args := []string{}
-        if len(cleaned) > 1 {
-            args = cleaned[1:]
-        }
-       
-        availableCommands := getCommands()
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		commandName := cleaned[0]
+		args := []string{}
+		if len(cleaned) > 1 {
+			args = cleaned[1:]
+		}
 
-        command, ok := availableCommands[commandName]
-        if !ok {
-            fmt.Println("invalid command")
-            continue
-        }
-        err := command.callback(cfg, args...)
-        if err != nil {
-            fmt.Println(err)
-        }
-    }
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("invalid command")
+			continue
+		}
+		err := command.callback(cfg, args...)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
 
 type cliCommand struct {
-    name        string
-    description string
-    callback    func(*config, ...string) error
+	name        string
+	description string
+	callback    func(*config, ...string) error
 }
 
-func getCommands() map[string] cliCommand {
-    return map[string] cliCommand {
-        "help": {
-            name: "help",
-            description: "Prints the help menu",
-            callback: callbackHelp, 
-        },
-        "map": {
-            name: "map",
-            description: "Lists next page of location areas",
-            callback: callbackMap, 
-        },
-        "mapb": {
-            name: "mapb",
-            description: "Lists previous page of location areas",
-            callback: callbackMapb, 
-        },
-        "explore": {
-            name: "explore {location_area}",
-            description: "Lists the pokemon in location areas",
-            callback: callbackExplore, 
-        },
-        "exit": {
-            name: "exit",
-            description: "Turns off pokedex",
-            callback: callbackExit,
-        },
-    }
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Lists next page of location areas",
+			callback:    callbackMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Lists previous page of location areas",
+			callback:    callbackMapb,
+		},
+		"explore": {
+			name:        "explore {location_area}",
+			description: "Lists the pokemon in location areas",
+			callback:    callbackExplore,
+		},
+		"catch": {
+			name:        "catch {pokemon_name}",
+			description: "Attempt to catch a pokemon and add it to your pokedex",
+			callback:    callbackCatch,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Turns off pokedex",
+			callback:    callbackExit,
+		},
+	}
 }
-
 
 func cleanInput(str string) []string {
-    lowered :=  strings.ToLower(str)
-    words := strings.Fields(lowered)
-    return words
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
 }
